fa0694fd8e2cf705cea7f5fbf89f6821ff04a845
Volodymyr Yahello <vyahello@gmail.com>
2020-01-04 20:28:17 +0200

    Add 'count duplicates' kata

---
+++ kata/06/count_duplicates.py
@@ -0,0 +1,30 @@
+"""
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that
+occur more than once in the input string. The input string can be assumed to contain only alphabets
+(both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+"""
+
+
+def duplicate_count(text: str) -> int:
+    """Counts amount of duplicates in a text.
+
+    Examples:
+        >>> assert duplicate_count("abcde") == 0
+        >>> assert duplicate_count("abcdea") == 1
+        >>> assert duplicate_count("indivisibility") == 1
+    """
+    return len(set(item for item in text.lower() if text.lower().count(item) > 1))
+
+
+if __name__ == "__main__":
+    print(duplicate_count("abcde"))
