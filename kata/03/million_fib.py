81d3e1e54ae3552eb8f151fa8f53bcc180ec2371
Volodymyr Yahello <vyahello@gmail.com>
2020-01-21 15:05:18 +0200

    Add millionth kata

--- kata/03/million_fib.py
+++ kata/03/million_fib.py
@@ -5,7 +5,7 @@ fib(0) := 0
 fib(1) := 1
 fin(n + 2) := fib(n + 1) + fib(n)
 """
-from typing import Generator
+from typing import Generator, Sequence
 from itertools import islice
 
 
@@ -18,5 +18,37 @@ def generator(number: int) -> Generator[int, None, None]:
 
 
 def fib(number: int) -> int:
-    """Calculates the nth Fibonacci number"""
+    """Calculates the nth Fibonacci number.
+
+    Examples:
+        >>> assert fib(10) == 55
+    """
     return next(islice(generator(number), number, number + 1))
+
+
+def calculate(number: int) -> Sequence[int]:
+    if number == 0:
+        return 0, 1
+    elif number == 1:
+        return 1, 1
+    else:
+        first, second = calculate(number // 2)
+        intermediate: int = first * (2 * second - first)
+        quit_: int = second * second + first * first
+        return (intermediate, quit_) if not number % 2 else (quit_, intermediate + quit_)
+
+
+def fib_efficient(number: int) -> int:
+    """Calculates the nth Fibonacci number (efficient).
+
+    Examples:
+         >>> assert fib_efficient(10) == 55
+    """
+    if number >= 0:
+        return calculate(number)[0]
+    return -calculate(-number)[0] if not number % 2 else calculate(-number)[0]
+
+
+if __name__ == "__main__":
+    print(fib(10))
+    print(fib_efficient(10))
