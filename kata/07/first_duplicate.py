e9f1da5214184a5095218359d12bfc327ec529f8
Volodymyr Yahello <vyahello@gmail.com>
2020-02-23 01:05:08 +0200

    Implement second version of "find first duplicate" kata

--- kata/07/first_duplicate.py
+++ kata/07/first_duplicate.py
@@ -24,5 +24,26 @@ def find_first_duplicate(sequence: Sequence[Any]) -> Any:
     return -1
 
 
+def find_first_duplicate_v2(sequence: Sequence[Any]) -> Any:
+    """Finds first duplicate in an array (high complexity).
+
+    Args:
+        sequence: an array
+
+    Returns:
+        first duplicate item
+
+    Examples:
+        >>> assert find_first_duplicate_v2("abccd") == "c"
+        >>> assert find_first_duplicate_v2([1, 2, 3, 4, 4, 5]) == 4
+    """
+    for top_index, top in enumerate(sequence):  # type: int, Any
+        for inner_index, inner in enumerate(sequence):  # type: int, Any
+            if top_index != inner_index and top == inner:
+                return top
+    return -1
+
+
 if __name__ == "__main__":
     print(find_first_duplicate("abccd"))
+    print(find_first_duplicate_v2("abccd"))
