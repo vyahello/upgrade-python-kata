878052fc71f4fc54ddd803f7a2e097b7b8420ac5
Volodymyr Yahello <vyahello@gmail.com>
2020-03-28 17:59:34 +0200

    Introduce 'Enumerable Magic #5- True for Just One?' kata

---
+++ kata/07/enumarete_magic.py
@@ -0,0 +1,27 @@
+"""
+Create a function called one that accepts two params:
+
+a sequence
+a function
+and returns true only if the function in the params returns true for exactly one item in the sequence.
+"""
+from typing import Sequence, Callable
+
+
+def one(sequence: Sequence[int], function: Callable[[int], bool]) -> bool:
+    """"Returns true for exactly one item in the sequence.
+
+    Args:
+        sequence (Sequence[int]): a sequence of numbers
+        function (Callable[[int], bool]): a function to call
+
+    Examples:
+        >>> assert one([1, 3, 5, 6, 99, 1, 3], lambda number: number > 10)
+        >>> assert not one([1, 3, 5, 6, 99, 88, 3], lambda number: number > 10)
+        >>> assert not one([1, 3, 5, 6, 5, 1, 3], lambda number: number > 10)
+    """
+    return len(tuple(filter(function, sequence))) == 1
+
+
+if __name__ == "__main__":
+    print(one([1, 3, 5, 6, 99, 1, 3], lambda number: number > 10))
