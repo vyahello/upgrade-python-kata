e0683ce6c6c901d5b53f1cf7b7af445b7b0fc809
Volodymyr Yahello <vyahello@gmail.com>
2020-03-29 11:01:38 +0300

    Implement pythonic 'Enumerate magic' kata

--- kata/07/enumarete_magic.py
+++ kata/07/enumarete_magic.py
@@ -23,5 +23,21 @@ def one(sequence: Sequence[int], function: Callable[[int], bool]) -> bool:
     return len(tuple(filter(function, sequence))) == 1
 
 
+def one_pythonic(sequence: Sequence[int], function: Callable[[int], bool]) -> bool:
+    """"Returns true for exactly one item in the sequence (pythonic).
+
+    Args:
+        sequence (Sequence[int]): a sequence of numbers
+        function (Callable[[int], bool]): a function to call
+
+    Examples:
+        >>> assert one_pythonic([1, 3, 5, 6, 99, 1, 3], lambda number: number > 10)
+        >>> assert not one_pythonic([1, 3, 5, 6, 99, 88, 3], lambda number: number > 10)
+        >>> assert not one_pythonic([1, 3, 5, 6, 5, 1, 3], lambda number: number > 10)
+    """
+    return sum(map(function, sequence)) == 1
+
+
 if __name__ == "__main__":
     print(one([1, 3, 5, 6, 99, 1, 3], lambda number: number > 10))
+    print(one_pythonic([1, 3, 5, 6, 99, 1, 3], lambda number: number > 10))
