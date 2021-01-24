9573e486edfac3ed9887c24c860b6ffeeb75438c
Volodymyr Yahello <vyahello@gmail.com>
2021-01-24 22:40:55 +0200

    Implement high to low classic solution

--- kata/07/high_to_low.py
+++ kata/07/high_to_low.py
@@ -11,7 +11,7 @@ high_and_low("1 9 3 4 -5") # return "9 -5"
 
 
 def high_and_low(numbers: str) -> str:
-    """
+    """Return min and max values.
 
     Args:
         numbers: <str> a list of numbers
@@ -25,5 +25,23 @@ def high_and_low(numbers: str) -> str:
     return f'{min(map(int, numbers.split()))} {max(map(int, numbers.split()))}'
 
 
+def high_and_low_classic(numbers: str) -> str:
+    """Return min and max values.
+
+    Examples:
+        >>> assert high_and_low_classic('1 2 3 4') == '1 4'
+
+    """
+    min_num = max_num = int(numbers[0])
+    for number in map(int, numbers.split()):  # type: int
+        if number < min_num:
+            min_num = number
+        if number > max_num:
+            max_num = number
+    return f'{min_num} {max_num}'
+
+
 if __name__ == '__main__':
     print(high_and_low('1 2 3 4'))
+    print(high_and_low_classic('1 2 3 4'))
+
